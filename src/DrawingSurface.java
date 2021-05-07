6c85b45c8657e7c0cc2c2164b943fa75b86b86a5
Harshith Pothula <70039379+hpothula983@users.noreply.github.com>
2021-05-07 14:11:49 -0700

    created empty window

--- src/DrawingSurface.java
+++ src/DrawingSurface.java
@@ -1,4 +1,37 @@
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+import processing.event.MouseEvent;
 
-public class DrawingSurface {
+public class DrawingSurface extends PApplet {
 
+		
+	// CONSTRUCTOR - Initialize any added fields here.
+	public DrawingSurface() 
+	{
+	}
+	
+	
+	// METHODS
+	// Add processing methods here. See the documentation at processing.org for reference.
+	public void setup() 
+	{
+	
+	}
+	
+	
+	public void draw()
+	{
+		background(255);
+		pushStyle();
+		
+		fill(0);
+		textSize(10);
+		text("TEST",10,10);
+		
+		popStyle();
+
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
 
 public class Main {
 
+	// MAIN - Typically in a class by itself, but included here for simplicity.
+	public static void main(String args[]) {
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+		window.setSize(600, 600);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+		window.setVisible(true);
+		canvas.requestFocus();
+	}
+
+	
 }
