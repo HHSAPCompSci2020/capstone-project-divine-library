7fbcf0520340bde4889b2a2b2839160da7e322f2
Harshith Pothula <70039379+hpothula983@users.noreply.github.com>
2021-05-14 13:59:21 -0700

    Update DrawingSurface.java

--- src/DrawingSurface.java
+++ src/DrawingSurface.java
@@ -14,8 +14,13 @@ import processing.event.MouseEvent;
 public class DrawingSurface extends PApplet {
 
 	private Library library;	
+<<<<<<< Updated upstream
 	private DrawingSurfaceShelves shelves;
 	private DrawingSurfaceMembers list;
+=======
+	private DrawingSurfaceShelves s;
+	private DrawingSurfaceMembers m;
+>>>>>>> Stashed changes
 	private PImage book, cd, mag;
 
 	// CONSTRUCTOR - Initialize any added fields here.
@@ -41,56 +46,56 @@ public class DrawingSurface extends PApplet {
 		background(255);
 		pushStyle();
 		
-		image(book,                 (float) 0, (float) (width/5.0),  (float)(width/3.0), (float)(height/3.0));
-		image(  cd,        (float)(width/3.0), (float) (width/5.0),  (float)(width/3.0), (float)(height/3.0));
-		image( mag, (float) (2.0*(width/3.0)), (float) (width/5.0),  (float)(width/3.0), (float)(height/3.0));
+		image(book,                 (float) 0, (float) (height/4.0),  (float)(width/3.0), (float)(height/3.0));
+		image(  cd,        (float)(width/3.0), (float) (height/4.0),  (float)(width/3.0), (float)(height/3.0));
+		image( mag, (float) (2.0*(width/3.0)), (float) (height/4.0),  (float)(width/3.0), (float)(height/3.0));
 		
 		textSize(height/12);
 		textAlign(CENTER);
 		fill(0);
-		text("Books",                        (float) (width/6.0), (float) (width/10.0));
-		text("DVDs",                         (float) (width/2.0), (float) (width/10.0));
-		text("Newspapers \n& Magazines", (float) (5*(width/6.0)), (float) (width/10.0));
+		text("Books",                        (float) (width/6.0), (float) (height/10.0));
+		text("DVDs",                         (float) (width/2.0), (float) (height/10.0));
+		text("Newspapers \n& Magazines", (float) (5*(width/6.0)), (float) (height/10.0));
 		    
 		noFill();
 		strokeWeight(3);
-		rect((float) 0, (float) (width/5.0),  (float)(width/3.0), (float)(height/3.0));
-		rect((float)(width/3.0), (float) (width/5.0),  (float)(width/3.0), (float)(height/3.0));
-		rect((float) (2.0*(width/3.0)), (float) (width/5.0),  (float)(width/3.0), (float)(height/3.0));
+		rect((float) 0,                 (float) (height/4.0),  (float)(width/3.0), (float)(height/3.0));
+		rect((float)(width/3.0),        (float) (height/4.0),  (float)(width/3.0), (float)(height/3.0));
+		rect((float) (2.0*(width/3.0)), (float) (height/4.0),  (float)(width/3.0), (float)(height/3.0));
 		
 		popStyle();
-
+		
 	}
 	
 	public void mousePressed() {
+		float x = mouseX;
+		float y = mouseY;
+		
+		if((x>0) && (y>height/4) && (x<width/3) && (y<(height/5 +height/3))) {
+			s = new DrawingSurfaceShelves(library.getShelves().getBookShelf());
+		}
+		else if ((x>width/3) && (y>(height/5 + height/3) && (x < (2*width/3)) && (y<(height/5 +height/3)))) {
+			s = new DrawingSurfaceShelves(library.getShelves().getDVDShelf());
+		}
+		else if ((x>(2*width/3) && (y>(height/5 + (2*(height/3))) && (x<width) && (y<height/5 +height/3 )))){
+			s = new DrawingSurfaceShelves(library.getShelves().getMagazineShelf());
+		}
+		
+		if (s != null) {
+			PApplet.runSketch(new String[]{""}, s);
+			PSurfaceAWT surf2 = (PSurfaceAWT) s.getSurface();
+			PSurfaceAWT.SmoothCanvas canvas2 = (PSurfaceAWT.SmoothCanvas) surf2.getNative();
+			JFrame window2 = (JFrame)canvas2.getFrame();
+
+			window2.setBounds(500,50,800, 600);
+			window2.setMinimumSize(new Dimension(100,100));
+			window2.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+			window2.setResizable(true);
+			canvas2.requestFocus();
+
+		}
 		
 	}
-	public void keyPressed() {
-//		if (super.keyCode == KeyEvent.VK_SPACE && m == null) {
-//
-//			m = new DrawingSurfaceShelves();
-//			
-//			PApplet.runSketch(new String[]{""}, m);
-//			PSurfaceAWT surf2 = (PSurfaceAWT) m.getSurface();
-//			PSurfaceAWT.SmoothCanvas canvas2 = (PSurfaceAWT.SmoothCanvas) surf2.getNative();
-//			JFrame window2 = (JFrame)canvas2.getFrame();
-//
-//			window2.setBounds(500,50,800, 600);
-//			window2.setMinimumSize(new Dimension(100,100));
-//			window2.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-//			window2.setResizable(true);
-//			canvas2.requestFocus();
-//			
-//			
-//		} else if (super.keyCode == KeyEvent.VK_SPACE) {
-//			
-//			PSurfaceAWT surf2 = (PSurfaceAWT) m.getSurface();
-//			PSurfaceAWT.SmoothCanvas canvas2 = (PSurfaceAWT.SmoothCanvas) surf2.getNative();
-//			JFrame window2 = (JFrame)canvas2.getFrame();
-//			
-//			window2.setVisible(!window2.isVisible());
-//			
-//		}
-	}
-
+	
+	
 }
